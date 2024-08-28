727e539ff695d165aa40a0b18c2ca3a1b643b582
francarrascoo <fr.carrascol@duocuc.cl>
2024-08-27 22:33:43 -0400

    ameeeee

--- graficos.py
+++ graficos.py
@@ -1,21 +1,19 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
 
 x = np.arange(0, 45,0.01) #Rango de X
-
 y = 200*x+700 #Función
 
 plt.plot(x, y) #Gráfico de lineas x e y / Ejes
 
-plt.axis([0, 45, 700, 9700]) # domino y recorrido
+plt.axis([0, 45, 700, 9700]) # Domino y recorrido
 
-plt.xlabel('Energía utilizada (kWh)')# con un título eje x
+plt.xlabel('Energía utilizada (kWh)')# Con un título eje x
 
-plt.ylabel('Costo para usuario ($)')# y un título de eje y
+plt.ylabel('Costo para usuario ($)')# Y un título de eje y
 
-plt.title('Costo para el usuario según uso de energía')# y un titulo del grafico
+plt.title('Costo para el usuario según uso de energía')# Y un titulo del grafico
 
-plt.grid()# pedimos mostrar una grilla para facilitar la lectura
+plt.grid()# Pedimos mostrar una grilla para facilitar la lectura
 
 plt.show()# y que nos muestre el gráfico
--- valor_ecuacion.py
+++ valor_ecuacion.py
@@ -1,52 +1,32 @@
-# Se llama a la biblioteca numpy, para trabajar con arreglos
-
-# de datos
+# Se llama a la biblioteca numpy, para trabajar con arreglos de datos
 
 import numpy as np
 
-# Modulo de la biblioteca scipy que nos permitir ́a poder
-
-# resolver ecuaciones no necesariamente lineales
-
-# a trav ́es de la funci ́on fsolve
+# Modulo de la biblioteca scipy que nos permitirá poder resolver ecuaciones no necesariamente lineales
+# a través de la función fsolve
 
 from scipy.optimize import fsolve
 
-# Se define la funci ́on C(e) y se retorna la expresi ́on
-
-# resultante del igualar a cero
+# Se define la función C(e) y se retorna la expresión resultante del igualar a cero
 
 def C(e):
     return 120*e+2000-68000
 
 # Lo que retorna proviene del siguiente despeje:
-
 # 120*e+\num{2000} = \num{68000}
-
 # 120*e+\num{2000}\num{-68000} = 0
 
-# Se le asigna a un arreglo t valores desde 0 hasta 1000
-
-# con un espacio de distancia. Por lo tanto, este vector
-
-# contiene solo el elemento 0
+# Se le asigna a un arreglo t valores desde 0 hasta 1000 con un espacio de distancia. 
+# Por lo tanto, este vector contiene solo el elemento 0
 
 t = np.linspace(0, 10000, 1)
 
-# Se le asigna a un arreglo sol, la(s) soluci ́on(es) entre
-
-# la expresi ́on retornada en def y el arreglo t.
-
-# Como este ́ultimo arreglo contiene solo el elemento 0,
-
-# entonces fsolve busca la soluci ́on entre
-
+# Se le asigna a un arreglo sol, la(s) solución(es) entre la expresión retornada en def y el arreglo t.
+# Como este ́ultimo arreglo contiene solo el elemento 0, entonces fsolve busca la solución entre
 # la expresi ́on 120*e+2000-68000 y 0
 
 sol = fsolve(C, t)
 
-# Imprime en la consola el resultado obtenido del despeje
-
-# de la ecuaci ́on
+# Imprime en la consola el resultado obtenido del despeje de la ecuación
 
 print(sol[0])
